1aa3f998804b902610c908423b37fa285097f1c4
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2025-03-06 22:06:21 -0300

    ADD - Repositories

---
+++ src/main/java/com/eventstec/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.eventstec.api.repositories;
+
+import com.eventstec.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/eventstec/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.eventstec.api.repositories;
+
+import com.eventstec.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
