f5d8e5e97ccfadbe8125d12e85c883ec6a0da5e7
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2025-03-06 22:03:38 -0300

    ADD - Create Entities

---
+++ src/main/java/com/eventstec/api/domain/address/Address.java
@@ -0,0 +1,23 @@
+package com.eventstec.api.domain.address;
+
+import com.eventstec.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
