eba3d273454640ef600c205d05f1288535163d7c
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2025-03-08 12:05:32 -0300

    ADD - List Events with Pagination

--- src/main/java/com/eventstec/api/controllers/EventController.java
+++ src/main/java/com/eventstec/api/controllers/EventController.java
@@ -2,11 +2,14 @@ package com.eventstec.api.controllers;
 
 import com.eventstec.api.domain.event.dtos.EventRequestDTO;
 import com.eventstec.api.domain.event.Event;
+import com.eventstec.api.domain.event.dtos.EventResponseDTO;
 import com.eventstec.api.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/events")
@@ -30,6 +33,13 @@ public class EventController {
         Event event = this.eventService.createEvent(eventRequestDTO);
 
         return ResponseEntity.ok(event);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> events = eventService.getAll(page, size);
+
+        return ResponseEntity.ok(events);
     }
 
 }
--- src/main/java/com/eventstec/api/services/EventService.java
+++ src/main/java/com/eventstec/api/services/EventService.java
@@ -2,9 +2,13 @@ package com.eventstec.api.services;
 
 import com.eventstec.api.domain.event.dtos.EventRequestDTO;
 import com.eventstec.api.domain.event.Event;
+import com.eventstec.api.domain.event.dtos.EventResponseDTO;
 import com.eventstec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,6 +19,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -51,6 +56,14 @@ public class EventService {
         eventRepository.save(newEvent);
 
         return newEvent;
+    }
+
+    public List<EventResponseDTO> getAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.eventRepository.findAll(pageable);
+
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.getRemote(), event.getEventUrl(), event.getImageUrl()))
+                .stream().toList();
     }
 
     private String uploadImg(MultipartFile multipartFile) {
